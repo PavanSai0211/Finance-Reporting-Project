5ec22db9c20d1b9e177192c9249f098482af8793
Pavan <pavansaipatel.2002@gmail.com>
2025-03-25 11:14:43 +0530

    removed few comments

--- bigQuery.py
+++ bigQuery.py
@@ -9,18 +9,17 @@ from fact_dim_creation import fact_financials, dim_company,dim_dividends,dim_loc
 
 
 
-# Load environment variables from .env file
+
 load_dotenv()
 
-# Fetch variables
+
 project_id = os.getenv("PROJECT_ID")
 dataset_id = os.getenv("DATASET_ID")
 email_password = os.getenv("EMAIL_PASSWORD")
 
-# Set up BigQuery client
+
 client = bigquery.Client(project=project_id)
 
-# Dictionary of dimension tables
 tables = {
     'fact_financials': fact_financials,
     'dim_company': dim_company,
@@ -32,6 +31,6 @@ tables = {
 for table_name, df in tables.items():
     to_gbq(df, f"{dataset_id}.{table_name}", project_id=project_id, if_exists="replace")
 
-# Upload all tables to BigQuery
+
 for tab_name, df_name in tables.items():
     pandas_gbq.to_gbq(df_name, f'{dataset_id}.{tab_name}', project_id=project_id, if_exists='replace')
